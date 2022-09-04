d706c76fcb7251a7738319e54328c8340d7cb9b9
Petr <peter.petushkov@inftix.com>
2022-09-04 21:28:25 +0400

    fix comment dark add 2

--- src/main/resources/static/js/item.js
+++ src/main/resources/static/js/item.js
@@ -177,7 +177,7 @@ function render(message, from, createdAt, id, language ) {
     }
 
     if(language == "ru") {
-        commentTemplate =     '                <div class=' + "comment comment rounded mb-2 pl-2 pb-2 pt-2"+styleClass + '>\n' +
+        commentTemplate =     '                <div class="comment comment rounded mb-2 pl-2 pb-2 pt-2 ' +styleClass + '">\n' +
             '                <div><b>От: </b>' + from + '</div>\n' +
             '                <div><b>Дата: </b>' + createdAt + '</div>\n' +
             '                <div>' + message + '</div>\n' +
@@ -189,7 +189,7 @@ function render(message, from, createdAt, id, language ) {
             '                </div>\n'
 
     } else {
-        commentTemplate =     '                <div class=' + "comment comment rounded mb-2 pl-2 pb-2 pt-2"+styleClass + '>\n' +
+        commentTemplate =     '                <div class="comment comment rounded mb-2 pl-2 pb-2 pt-2 ' +styleClass + '">\n' +
             '                <div><b>From: </b>' + from + '</div>\n' +
             '                <div><b>Date: </b>' + createdAt + '</div>\n' +
             '                <div>' + message + '</div>\n' +
