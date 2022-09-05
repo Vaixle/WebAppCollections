67ec2ffa1fee9333d2c334bf38f67592cd9529a5
Petr <peter.petushkov@inftix.com>
2022-09-05 14:13:18 +0400

    update style for search

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -4,3 +4,41 @@ $("#search").on('keypress',function(e) {
         e.preventDefault();
     }
 });
+
+$('#lang').on('change', function (e) {
+
+    let valueSelected = this.value;
+
+    let actionUrl = window.document.location.origin + window.document.location.pathname
+
+    window.location.replace(actionUrl +"?lang="+valueSelected)
+});
+
+$('#change-style').on('change', changeStyle);
+
+function changeStyle(e) {
+
+    let valueSelected = this.value;
+
+    let actionUrl = window.document.location.origin + window.document.location.pathname
+
+    $.ajax({
+        type: "GET",
+        data:{"style" : valueSelected},
+        url: actionUrl,
+    });
+
+    $('body').toggleClass('gradient-dark')
+
+    $('button').toggleClass('btn-info')
+
+    $('div.bg-light').toggleClass('bg-dark')
+
+    $('div.bg-light').toggleClass('text-white')
+
+}
+
+
+if ($('#change-style').val() == "dark") {
+    changeStyle()
+}
